296dac116d59b2561ed31412b552dac04fd0ea3b
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2021-06-21 23:58:10 -0400

    Optimized the database, storing only the unique user id.

--- db/dbInit.js
+++ db/dbInit.js
@@ -12,10 +12,6 @@ const Users = require('../models/Users.js')(sequelize, Sequelize.DataTypes);
 const force = process.argv.includes('--force') || process.argv.includes('-f');
 
 sequelize.sync({ force }).then(async () => {
-	const users = [
-		Users.upsert({ userid: 848611967748014130, username: 'jaytest', usertag: 5817}),
-	];
-	await Promise.all(users);
-	console.log('Database synced');
+	console.log('Database successfully synced/reset!');
 	sequelize.close();
 }).catch(console.error);
--- models/Users.js
+++ models/Users.js
@@ -1,11 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Users', {
         userid: {
-          type: DataTypes.INTEGER,
+          type: DataTypes.STRING,
           primaryKey: true,
         },
-        username: DataTypes.STRING,
-        usertag: DataTypes.INTEGER,
         minutes: {
           type: DataTypes.INTEGER,
           defaultValue: 0,
